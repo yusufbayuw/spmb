b0fcf6d5d5940d972b80d663db81559aef5e9d42
yusufbayuw <yusufbayu31@gmail.com>
2026-09-02 23:20:59 +0700

    feat: add audit trail list page

---
+++ app/Filament/Admin/Resources/AuditLogResource/Pages/ListAuditLogs.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Admin\Resources\AuditLogResource\Pages;
+
+use App\Filament\Admin\Resources\AuditLogResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAuditLogs extends ListRecords
+{
+    protected static string $resource = AuditLogResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+}
