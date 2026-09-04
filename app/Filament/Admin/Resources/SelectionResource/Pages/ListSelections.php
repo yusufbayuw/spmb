e27ac9be46b765b77d5e372a114a8100ae91b397
yusufbayuw <yusufbayu31@gmail.com>
2026-09-04 16:20:18 +0700

    Remove manual selection creation entry point

--- app/Filament/Admin/Resources/SelectionResource/Pages/ListSelections.php
+++ app/Filament/Admin/Resources/SelectionResource/Pages/ListSelections.php
@@ -1,3 +1,11 @@
 <?php
-namespace App\Filament\Admin\Resources\SelectionResource\Pages;use App\Filament\Admin\Resources\SelectionResource;use Filament\Actions;use Filament\Resources\Pages\ListRecords;
-class ListSelections extends ListRecords{protected static string $resource=SelectionResource::class;protected function getHeaderActions():array{return [Actions\CreateAction::make()];}}
+
+namespace App\Filament\Admin\Resources\SelectionResource\Pages;
+
+use App\Filament\Admin\Resources\SelectionResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListSelections extends ListRecords
+{
+    protected static string $resource = SelectionResource::class;
+}
