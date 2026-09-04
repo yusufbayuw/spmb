a4cd1a84427766965d82e58576531b2741fdecd4
yusufbayuw <yusufbayu31@gmail.com>
2026-09-04 16:20:48 +0700

    Remove manual announcement creation entry point

--- app/Filament/Admin/Resources/AnnouncementResource/Pages/ListAnnouncements.php
+++ app/Filament/Admin/Resources/AnnouncementResource/Pages/ListAnnouncements.php
@@ -1,3 +1,11 @@
 <?php
-namespace App\Filament\Admin\Resources\AnnouncementResource\Pages;use App\Filament\Admin\Resources\AnnouncementResource;use Filament\Actions;use Filament\Resources\Pages\ListRecords;
-class ListAnnouncements extends ListRecords{protected static string $resource=AnnouncementResource::class;protected function getHeaderActions():array{return [Actions\CreateAction::make()];}}
+
+namespace App\Filament\Admin\Resources\AnnouncementResource\Pages;
+
+use App\Filament\Admin\Resources\AnnouncementResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAnnouncements extends ListRecords
+{
+    protected static string $resource = AnnouncementResource::class;
+}
