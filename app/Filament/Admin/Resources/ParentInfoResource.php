a0abc2d32db218286904f4394821bd0cf7d8305b
yusufbayuw <yusufbayu31@gmail.com>
2026-09-07 10:16:37 +0700

    Remove duplicate parent data item from sidebar

--- app/Filament/Admin/Resources/ParentInfoResource.php
+++ app/Filament/Admin/Resources/ParentInfoResource.php
@@ -28,6 +28,11 @@ class ParentInfoResource extends Resource
 
     protected static ?int $navigationSort = 3;
 
+    public static function shouldRegisterNavigation(): bool
+    {
+        return false;
+    }
+
     public static function form(Form $form): Form
     {
         return $form->schema([
