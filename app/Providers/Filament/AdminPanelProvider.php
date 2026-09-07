62b730816466ff14b973dd96cf926f37a19cd30f
yusufbayuw <yusufbayu31@gmail.com>
2026-09-07 10:27:38 +0700

    Simplify admin navigation groups around operational workflow

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,7 +36,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\\Filament\\Admin\\Widgets')
             ->widgets([Widgets\AccountWidget::class])
             ->plugins([FilamentShieldPlugin::make()])
-            ->navigationGroups(['Pendaftaran','Verifikasi','Seleksi & Pengumuman','Pasca-Pengumuman','Laporan','Konfigurasi SPMB','Sistem & Akses'])
+            ->navigationGroups(['Pendaftaran','Pasca-Pengumuman','Laporan','Konfigurasi SPMB','Sistem & Akses'])
             ->sidebarCollapsibleOnDesktop()
             ->renderHook(
                 PanelsRenderHook::BODY_END,
