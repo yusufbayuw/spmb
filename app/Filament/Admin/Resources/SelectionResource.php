4c6d4e560cf118b6c390419f8d28892aa015e95a
yusufbayuw <yusufbayu31@gmail.com>
2026-09-07 10:25:30 +0700

    Continue registration workflow navigation order

--- app/Filament/Admin/Resources/SelectionResource.php
+++ app/Filament/Admin/Resources/SelectionResource.php
@@ -25,9 +25,9 @@ class SelectionResource extends Resource
 
     protected static ?string $pluralModelLabel = 'Penetapan Hasil';
 
-    protected static ?string $navigationGroup = 'Seleksi & Pengumuman';
+    protected static ?string $navigationGroup = 'Pendaftaran';
 
-    protected static ?int $navigationSort = 4;
+    protected static ?int $navigationSort = 6;
 
     public static function form(Form $form): Form
     {
