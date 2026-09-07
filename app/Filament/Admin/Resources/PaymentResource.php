222b8e4984064fc681afe13472538be917dada30
yusufbayuw <yusufbayu31@gmail.com>
2026-09-07 10:16:34 +0700

    Clarify payment verification navigation label

--- app/Filament/Admin/Resources/PaymentResource.php
+++ app/Filament/Admin/Resources/PaymentResource.php
@@ -20,7 +20,7 @@ class PaymentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-banknotes';
 
-    protected static ?string $navigationLabel = 'Pembayaran';
+    protected static ?string $navigationLabel = 'Verifikasi Pembayaran';
 
     protected static ?string $navigationGroup = 'Verifikasi';
 
