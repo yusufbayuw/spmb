703eaf7eab07eee0062e4bb23efb87fe26be01a2
yusufbayuw <yusufbayu31@gmail.com>
2026-09-07 10:14:11 +0700

    Move setup resources into SPMB configuration

--- app/Filament/Admin/Resources/AdmissionTestResource.php
+++ app/Filament/Admin/Resources/AdmissionTestResource.php
@@ -21,9 +21,9 @@ class AdmissionTestResource extends Resource
 
     protected static ?string $navigationLabel = 'Konfigurasi Tes';
 
-    protected static ?string $navigationGroup = 'Seleksi';
+    protected static ?string $navigationGroup = 'Konfigurasi SPMB';
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 5;
 
     public static function form(Form $form): Form
     {
