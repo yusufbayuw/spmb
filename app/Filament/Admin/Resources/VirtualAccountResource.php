930bb3242105670f96826cebde6d345148f6592b
yusufbayuw <yusufbayu31@gmail.com>
2026-09-07 10:27:26 +0700

    Order SPMB configuration by setup dependency

--- app/Filament/Admin/Resources/VirtualAccountResource.php
+++ app/Filament/Admin/Resources/VirtualAccountResource.php
@@ -23,7 +23,7 @@ class VirtualAccountResource extends Resource
 
     protected static ?string $navigationGroup = 'Konfigurasi SPMB';
 
-    protected static ?int $navigationSort = 4;
+    protected static ?int $navigationSort = 5;
 
     public static function table(Table $table): Table
     {
