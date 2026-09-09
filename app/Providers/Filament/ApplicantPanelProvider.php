2a156994a673b62473cdd7fbfd621a804b136967
yusufbayuw <yusufbayu31@gmail.com>
2026-09-09 10:48:58 +0700

    Use applicant login backgrounds

--- app/Providers/Filament/ApplicantPanelProvider.php
+++ app/Providers/Filament/ApplicantPanelProvider.php
@@ -20,6 +20,8 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Swis\Filament\Backgrounds\FilamentBackgroundsPlugin;
+use Swis\Filament\Backgrounds\ImageProviders\MyImages;
 
 class ApplicantPanelProvider extends PanelProvider
 {
@@ -50,6 +52,10 @@ class ApplicantPanelProvider extends PanelProvider
             ])
             ->widgets([
                 AccountWidget::class,
+            ])
+            ->plugins([
+                FilamentBackgroundsPlugin::make()
+                    ->imageProvider(MyImages::make()->directory('images/login-pendaftar')),
             ])
             ->sidebarCollapsibleOnDesktop()
             ->renderHook(
