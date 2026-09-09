e51f3c334efab6883e9489db53988176cffd135d
yusufbayuw <yusufbayu31@gmail.com>
2026-09-09 11:28:25 +0700

    Implement local captcha auth protection

---
+++ app/Filament/Applicant/Pages/Auth/Login.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Applicant\Pages\Auth;
+
+use App\Filament\Support\SpmbCaptcha;
+use Filament\Forms\Form;
+use Filament\Pages\Auth\Login as BaseLogin;
+
+class Login extends BaseLogin
+{
+    public function form(Form $form): Form
+    {
+        $form = parent::form($form);
+
+        return $form->schema([
+            ...$form->getComponents(),
+            SpmbCaptcha::make(),
+        ]);
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        $this->data['captcha'] = null;
+
+        parent::throwFailureValidationException();
+    }
+}
