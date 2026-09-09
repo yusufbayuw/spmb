0af935fc5500ac272c7d25e8598fcd24b4df8ae6
yusufbayuw <yusufbayu31@gmail.com>
2026-09-09 11:30:19 +0700

    Handle captcha state across auth validation failures

--- app/Filament/Applicant/Pages/Auth/RequestPasswordReset.php
+++ app/Filament/Applicant/Pages/Auth/RequestPasswordReset.php
@@ -2,12 +2,16 @@
 
 namespace App\Filament\Applicant\Pages\Auth;
 
+use App\Filament\Support\Concerns\ResetsCaptchaOnValidationError;
 use App\Filament\Support\SpmbCaptcha;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Pages\Auth\PasswordReset\RequestPasswordReset as BaseRequestPasswordReset;
 
 class RequestPasswordReset extends BaseRequestPasswordReset
 {
+    use ResetsCaptchaOnValidationError;
+
     public function form(Form $form): Form
     {
         $form = parent::form($form);
@@ -16,5 +20,12 @@ class RequestPasswordReset extends BaseRequestPasswordReset
             ...$form->getComponents(),
             SpmbCaptcha::make(),
         ]);
+    }
+
+    protected function getFailureNotification(string $status): ?Notification
+    {
+        $this->data['captcha'] = null;
+
+        return parent::getFailureNotification($status);
     }
 }
