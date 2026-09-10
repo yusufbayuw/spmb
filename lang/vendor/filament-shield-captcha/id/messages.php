6d976ec55c2b0069da70c8a37dd7d47103361593
yusufbayuw <yusufbayu31@gmail.com>
2026-09-10 09:47:35 +0700

    Localize captcha validation messages

---
+++ lang/vendor/filament-shield-captcha/id/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'actions' => [
+        'refresh' => 'Muat captcha baru',
+    ],
+    'alt' => 'Gambar kode keamanan',
+    'direction' => 'ltr',
+    'validation' => [
+        'failed' => 'Kode keamanan tidak sesuai. Silakan coba lagi.',
+    ],
+];
