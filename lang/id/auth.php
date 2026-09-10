0950354801ac88992b2df81e992f765553bf5ef7
yusufbayuw <yusufbayu31@gmail.com>
2026-09-10 09:45:27 +0700

    Localize validation and authentication messages

---
+++ lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Email atau kata sandi yang Anda masukkan tidak benar.',
+    'password' => 'Kata sandi yang dimasukkan tidak benar.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+];
