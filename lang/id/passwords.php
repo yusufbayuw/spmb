d39152b57675c196ceb57c2b9c9e1ec619f4249b
yusufbayuw <yusufbayu31@gmail.com>
2026-09-10 09:45:32 +0700

    Localize validation and authentication messages

---
+++ lang/id/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Kata sandi berhasil diubah.',
+    'sent' => 'Tautan pengaturan ulang kata sandi telah dikirim.',
+    'throttled' => 'Silakan tunggu sebelum mencoba kembali.',
+    'token' => 'Token pengaturan ulang kata sandi tidak valid atau sudah kedaluwarsa.',
+    'user' => 'Akun dengan alamat email tersebut tidak ditemukan.',
+];
