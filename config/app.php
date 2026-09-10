532744278eaed6116195b484932773acfd2dc961
yusufbayuw <yusufbayu31@gmail.com>
2026-09-10 09:46:58 +0700

    Make Indonesian the authoritative application locale

--- config/app.php
+++ config/app.php
@@ -78,11 +78,11 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'id'),
+    'locale' => 'id',
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'id_ID'),
+    'faker_locale' => 'id_ID',
 
     /*
     |--------------------------------------------------------------------------
