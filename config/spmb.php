498905d0e782738e03f507d1eb5faa9483155b36
yusufbayuw <yusufbayu31@gmail.com>
2026-09-15 15:18:36 +0700

    feat: add public admissions portal settings

--- config/spmb.php
+++ config/spmb.php
@@ -1,6 +1,18 @@
 <?php
 
 return [
+    'portal' => [
+        'name' => env('SPMB_PORTAL_NAME', 'Portal Penerimaan Taruna Bakti'),
+        'foundation_name' => env('SPMB_FOUNDATION_NAME', 'Yayasan Taruna Bakti'),
+        'foundation_website' => env('SPMB_FOUNDATION_WEBSITE'),
+        'foundation_email' => env('SPMB_FOUNDATION_EMAIL'),
+        'foundation_phone' => env('SPMB_FOUNDATION_PHONE'),
+        'foundation_whatsapp' => env('SPMB_FOUNDATION_WHATSAPP'),
+        'foundation_address' => env('SPMB_FOUNDATION_ADDRESS', 'Jl. L.L.R.E. Martadinata No. 52, Bandung'),
+        'service_hours' => env('SPMB_FOUNDATION_SERVICE_HOURS', 'Hari kerja 08.00–14.00 WIB'),
+        'logo_path' => env('SPMB_PORTAL_LOGO_PATH'),
+    ],
+
     'uploads' => [
         'max_kb' => (int) env('SPMB_UPLOAD_MAX_KB', 5120),
         'allowed_mimes' => [
