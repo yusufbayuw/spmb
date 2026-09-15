c47c618da79374a0aa87784c1a91d209f8d2986e
yusufbayuw <yusufbayu31@gmail.com>
2026-09-15 11:08:26 +0700

    feat: allow applicant login by username

--- app/Filament/Applicant/Pages/Auth/Login.php
+++ app/Filament/Applicant/Pages/Auth/Login.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Applicant\Pages\Auth;
 
+use App\Filament\Support\Concerns\AuthenticatesWithEmailOrUsername;
 use App\Filament\Support\Concerns\ResetsCaptchaOnValidationError;
 use App\Filament\Support\SpmbCaptcha;
 use Filament\Forms\Form;
@@ -9,6 +10,7 @@ use Filament\Pages\Auth\Login as BaseLogin;
 
 class Login extends BaseLogin
 {
+    use AuthenticatesWithEmailOrUsername;
     use ResetsCaptchaOnValidationError;
 
     public function form(Form $form): Form
