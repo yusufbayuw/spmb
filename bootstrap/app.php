9ad9c748b62ff436aff39d88e3d96bcec9c8e4a3
yusufbayuw <yusufbayu31@gmail.com>
2026-09-15 14:46:19 +0700

    feat: attach request reference ids globally

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AssignRequestId;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,11 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Hapus atau comment baris HandleInertiaRequests
-        // $middleware->web(append: [
-        //     \App\Http\Middleware\HandleInertiaRequests::class,
-        //     \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        // ]);
+        $middleware->prepend(AssignRequestId::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
