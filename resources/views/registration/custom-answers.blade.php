29d6d66065c0901d3b2ded476f9611f49d400611
yusufbayuw <yusufbayu31@gmail.com>
2026-09-25 17:49:43 +0700

    Show conditional boolean details in applicant summaries

--- resources/views/registration/custom-answers.blade.php
+++ resources/views/registration/custom-answers.blade.php
@@ -4,6 +4,10 @@
             @if($field['active'] && array_key_exists($field['key'], $registration->custom_answers ?? []))
                 @php
 $answer = $registration->custom_answers[$field['key']];
+$detailSettings = ($field['type'] ?? null) === 'boolean'
+    ? app(\App\Services\ConfiguredRegistrationForm::class)->booleanDetailSettings($field, $answer)
+    : null;
+$detailAnswer = data_get($registration->custom_answers, '_details.'.$field['key']);
 @endphp
                 <div>
                     <dt class="text-sm text-gray-500">{{ $field['label'] }}</dt>
@@ -22,6 +26,12 @@ $answer = $registration->custom_answers[$field['key']];
                             {{ is_array($answer) ? implode(', ', $answer) : ($field['type'] === 'boolean' ? ($answer ? 'Ya' : 'Tidak') : $answer) }}
                         @endif
                     </dd>
+                    @if($detailSettings && filled($detailAnswer))
+                        <div class="mt-3 border-l-2 border-gray-200 pl-3 dark:border-gray-700">
+                            <dt class="text-xs text-gray-500 dark:text-gray-400">{{ $detailSettings['label'] }}</dt>
+                            <dd class="mt-1 whitespace-pre-line text-sm text-gray-950 dark:text-white">{{ $detailAnswer }}</dd>
+                        </div>
+                    @endif
                 </div>
             @endif
         @endforeach
