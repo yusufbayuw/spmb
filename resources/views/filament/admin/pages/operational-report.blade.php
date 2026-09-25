a5e886844210a0de8ed44bead127053402d97e8b
yusufbayuw <yusufbayu31@gmail.com>
2026-09-25 14:13:02 +0700

    Keep operational report cards single-column on mobile

--- resources/views/filament/admin/pages/operational-report.blade.php
+++ resources/views/filament/admin/pages/operational-report.blade.php
@@ -23,7 +23,7 @@
             </div>
         </form>
 
-        <div class="grid gap-4 sm:grid-cols-2 xl:grid-cols-4">
+        <div class="grid gap-4 md:grid-cols-2 xl:grid-cols-4">
             @foreach ([
                 ['Total Pendaftaran', number_format($summary['total']), 'heroicon-o-users'],
                 ['Aktif', number_format($summary['active']), 'heroicon-o-play-circle'],
